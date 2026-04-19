5d85f19176ec82b04d0425bb0017d5d5f9fc2322
Anil Kumar <anilachary67@gmail.com>
2026-04-19 21:10:12 +0530

    Update configuration with new credentials and URLs

--- importer/config.py
+++ importer/config.py
@@ -1,9 +1,10 @@
 client_id = 'admin-cli'
 client_secret = ''
 username = 'admin'
-password = 'Keyclock@2026'
-access_token_url = 'https://wb.aarogya-aarohan.tanuh.ai/auth/realms/master/protocol/openid-connect/token'
-fhir_base_url = 'https://wb.aarogya-aarohan.tanuh.ai/fhir'
-keycloak_url = 'https://wb.aarogya-aarohan.tanuh.ai/auth/admin/realms/arogyam'
+password = 'Adroit@098'
+access_token_url = 'https://<Host>-prod.octf.artpark.ai/auth/realms/master/protocol/openid-connect/token'
+fhir_base_url = 'https://<Host>-prod.octf.artpark.ai/fhir'
+keycloak_url = 'https://<host>-prod.octf.artpark.ai/auth/admin/realms/arogyam'
 access_token = None
 product_access_token = 'example-pr̀̀oduct-access-token'
+
